56673e375737473461712de88a14194fb84f5f26
hebertcisco <hebertcisco@outlook.com>
2023-03-23 23:41:06 -0300

    update default-phrases.ts

--- src/shared/json/default-phrases.ts
+++ src/shared/json/default-phrases.ts
@@ -16,38 +16,6 @@ export default [
   },
   {
     author: 'Jesus Cristo',
-    text: 'De que serve ao homem conquistar o mundo inteiro se perder a alma?',
-    translated: 'What is the use of a man to conquer the whole world if he loses his soul?',
-  },
-  {
-    author: 'Jesus Cristo',
-    text: 'E conhecereis a verdade e a verdade vos libertará.',
-    translated: 'And you will know the truth, and the truth will set you free.',
-  },
-
-  {
-    author: 'Jesus Cristo',
-    text: 'Pai, perdoa-lhes, porque não sabem o que fazem.',
-    translated: 'Father, forgive them, for they do not know what they are doing.',
-  },
-  {
-    author: 'Jesus Cristo',
-    text: 'A boca fala do que está cheio o coração.',
-    translated: 'The mouth speaks what the heart is full of.',
-  },
-
-  {
-    author: 'Jesus Cristo',
-    text: 'Não julgueis segundo a aparência, e sim pela reta justiça.',
-    translated: 'Do not judge by appearances, but judge with right judgment.',
-  },
-  {
-    author: 'Jesus Cristo',
-    text: 'Amai-vos uns aos outros, como eu vos amei.',
-    translated: 'Love one another as I have loved you.',
-  },
-  {
-    author: 'Jesus Cristo',
     text: 'Não temas, eu venci o mundo!',
     translated: 'Do not be afraid, I have overcome the world!',
   },
